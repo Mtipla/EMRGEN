889391f3fab773a2bc3611a36dc70955a506af27
AlonsoFGJ <alo.gonzalezj@duocuc.cl>
2026-09-23 17:34:17 -0300

    Estabilización de versiones dentro del proyecto

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/packages/eslint-config/base.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicación/packages/eslint-config/base.js
@@ -1,8 +1,8 @@
-import babelParser from "@babel/eslint-parser";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import onlyWarn from "eslint-plugin-only-warn";
+import tseslint from "typescript-eslint";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -14,13 +14,7 @@ export const config = [
   eslintConfigPrettier,
   {
     languageOptions: {
-      parser: babelParser,
-      parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ["@babel/preset-typescript"],
-        },
-      },
+      parser: tseslint.parser,
     },
     plugins: {
       turbo: turboPlugin,
